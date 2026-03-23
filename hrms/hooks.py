635d15a978ee3c4595898bd66e61db64df2c987a
Francesco Magalini <francesco@francescomagalini.com>
2026-03-23 17:37:07 +0000

    feat(auth): enforce ameide oidc flow

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -82,7 +82,10 @@ website_generators = ["Job Opening"]
 website_route_rules = [
 	{"from_route": "/hrms/<path:app_path>", "to_route": "hrms"},
 	{"from_route": "/hr/<path:app_path>", "to_route": "roster"},
+	{"from_route": "/login", "to_route": "ameide_oidc"},
+	{"from_route": "/logout", "to_route": "ameide_oidc_logout"},
 	{"from_route": "/auth/ameide-oidc", "to_route": "ameide_oidc"},
+	{"from_route": "/auth/ameide-oidc/redirect", "to_route": "ameide_oidc_redirect"},
 	{"from_route": "/auth/ameide-oidc/logout", "to_route": "ameide_oidc_logout"},
 ]
 # Jinja
